bc64b5113bdf3cc66c2c499539221dd96e0209cc
Akshaj6 <akshaj.samaria@gmail.com>
2025-04-01 17:51:27 +0530

    12

---
+++ Car vs Bike.py
@@ -0,0 +1,13 @@
+inp = input("Do you want bike or car? ")
+if inp == "bike":
+    a = input("Scooter or Sport Bike? ")
+    if a == "Scooter":
+        print("Drive safely with a helmet.")
+    else:
+        print("Time to zoom to your destination!")
+if inp == "car":
+    b = input("Sedan or SUV? ")
+    if b == "Sedan":
+        print("That is a good choice")
+    else:
+        print("Your whole family can fit inside this SUV!")
